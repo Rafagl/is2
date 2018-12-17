71eeb08f4dec5c70e7683b6a70294db9fefb3d30
Rafagl <galanlopezrafael@gmail.com>
2018-12-17 10:29:36 +0100

    Add files via upload

--- src/components/foodContainer.jsx
+++ src/components/foodContainer.jsx
@@ -109,12 +109,46 @@ class foodContainer extends React.Component {
           ]
         })
     }
+
+    myFunction = id => {
+        var x = document.getElementById(id);
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+        }
+    }
     render() {
         if (this.state.productos !== null){
         return (
             <div>
                 <br/>
-                <button className="w3-button w3-blue w3-round-xlarge" onClick={this.handleOpen}>Buscar</button>
+                <button className="w3-button w3-blue w3-round-xlarge w3-margin" onClick={() => this.myFunction("options")}>Filtros</button>
+                <button className="w3-button w3-blue w3-round-xlarge" >Buscar</button>
+
+                <div id="options" className="w3-panel w3-round-large w3-topbar w3-bottombar w3-border-blue w3-light-gray w3-padding w3-hide" style={{ marginTop: '2%', margin: 'auto', width: '30%' }}>
+                        <h3>Nombre:</h3>
+                        <input type="text" className="w3-input w3-border w3-round"></input>
+                        <h3>Ingredientes:</h3>
+                        <input type="text" className="w3-input w3-border w3-round"></input>
+                        <h3>Alérgenos:</h3>
+                        <input type="text" className="w3-input w3-border w3-round"></input>
+                        <div className="frame w3-border w3-round-large">
+                            <h3>Ubicación:</h3>
+                            <label className="container" onChange={() => this.handleChange(2, "Madrid")}>Madrid
+                            <input type="checkbox" id="checkbox1"/>
+                                <span className="checkmark" ></span>
+                            </label>
+                            <label className="container">Barcelona
+                            <input type="checkbox" id="checkbox2" />
+                                <span className="checkmark"></span>
+                            </label>
+                        </div>
+                        <div className="frame w3-border w3-round-large">
+                            <h3>Fecha de elaboración:</h3>
+                            <input type="date" />
+                        </div>
+                    </div>
                 <PopUpFood open={this.state.showPopUp} onClose={this.handleClose} handleSubmit={this.handleSubmit} handleChange={this.handleChange} handleSearch={this.handleSearch}/>
                 <div className="food-grid-container">
                     <div className="food-grid">
